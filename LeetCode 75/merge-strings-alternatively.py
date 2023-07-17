d7bb7424b88815f3d89229c8ee8bb20db29c9e88
Markson17 <marksonfrosh@gmail.com>
2023-07-17 13:14:49 +0100

    added solution to merge stings alternatively task.

---
+++ LeetCode 75/merge-strings-alternatively.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+
+        # initialize the merged words to an epmty string
+        merged_words = ""
+
+        # store the minimum length in a container
+        length = min(len(word1), len(word2))
+
+        # loop through the range of length and append the words to the merged words
+        for i in range(length):
+            merged_words += word1[i] + word2[i]
+
+        # Append any remaining characters from the longer word
+        if len(word1) > length:
+            merged_words += word1[length:]
+        elif len(word2) > length:
+            merged_words += word2[length:]
+
+        # return merged words
+        return merged_words
