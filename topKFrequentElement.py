032c9e44288736c249ea794421a4f6ec2f65ecc2
Markson17 <marksonfrosh@gmail.com>
2024-05-05 23:18:27 +0100

    resumed leetcode grind

---
+++ topKFrequentElement.py
@@ -0,0 +1,16 @@
+def topKFrequent(nums, k):
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]
+    
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+    for n, c in count.items():
+        freq[c].append(n)
+
+    res = []
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+
