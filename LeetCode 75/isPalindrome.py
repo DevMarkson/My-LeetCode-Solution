d395e5cf7e7f625e5028bb402d808885a01e589d
Markson17 <marksonfrosh@gmail.com>
2024-05-12 15:00:01 +0100

    Refactor isPalindrome function for improved readability and performance

--- LeetCode 75/isPalindrome.py
+++ LeetCode 75/isPalindrome.py
@@ -1,8 +1,37 @@
-def isPalindrome(word):
-    if word == word[::-1]:
-        return True
-    else:
-        return False
+def isPalindrome(s):
+    l, r = 0, len(s) - 1
+
+    while l < r:
+        while l < r and not isAlphanum(s[l]):
+            l += 1
+        while r > l and not isAlphanum(s[r]):
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l = l + 1
+        r = r - 1
+    return True
+
+def isAlphanum(c):
+    return (ord('A') <= ord(c) <= ord('Z') or 
+        ord('a') <= ord(c) <= ord('z') or 
+        ord('0') <= ord(c) <= ord('9'))
+
+s = "A man, a plan, a canal: Panama"
+print(isPalindrome(s))
+        
+
+
+
+
+
+
+# def isPalindrome(word):
+#     if word == word[::-1]:
+#         return True
+#     else:
+#         return False
     
-word = "madams"
-print(isPalindrome(word))
+    
+# word = "madams"
+# print(isPalindrome(word))
