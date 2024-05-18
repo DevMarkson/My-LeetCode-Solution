2d2c0a09ac5c327494bf9ea2fa269224265ae02d
Markson17 <marksonfrosh@gmail.com>
2024-05-18 23:48:18 +0100

    added the solution for contain maximum water

---
+++ LeetCode 75/containMaxWater.py
@@ -0,0 +1,18 @@
+def ConatainMaxWater(height):
+    res = 0
+    l, r = 0, len(height) - 1
+
+    while l < r:
+        area = (r-l) * min(height[l], height[r])
+        res = max(res, area)
+
+        if height[l] < height[r]:
+            l += 1
+
+        else:
+            r -= 1
+
+    return res
+
+height = [1,8,6,2,5,4,8,3,7]
+print("The maximum area of the array is", ConatainMaxWater(height))
