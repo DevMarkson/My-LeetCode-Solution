54ba6476275683c3f94788f407b4878cb03218fe
Markson17 <marksonfrosh@gmail.com>
2024-05-30 23:47:34 +0100

    feat: Implement MinStack class with push, pop, top, and getMin methods

--- LeetCode 75/minStack.py
+++ LeetCode 75/minStack.py
@@ -1,14 +1,23 @@
 class MinStack(object):
     def __init__(self):
+        self.stack = []
+        self.minStack = []
 
     def push(self, val):
-        pass
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
 
-    def pop(self, val):
-        pass
+    def pop(self):
+        self.stack.pop()
+        self.minStack.pop()
 
-    def top(self, val):
-        pass
+    def top(self):
+        return self.stack[-1]
 
-    def minStack(self, val):
-        pass
+    def getMin(self):
+        return self.minStack[-1]
+    
+minStack = MinStack()
+val = [[],[-2],[0],[-3],[],[],[],[]]
+print(minStack.top(-2))
