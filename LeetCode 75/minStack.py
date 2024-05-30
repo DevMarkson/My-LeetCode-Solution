f0e90a00d604f5424a4cc38cc0435653081cb21f
Markson17 <marksonfrosh@gmail.com>
2024-05-30 23:35:56 +0100

    created the min stack file

---
+++ LeetCode 75/minStack.py
@@ -0,0 +1,14 @@
+class MinStack(object):
+    def __init__(self):
+
+    def push(self, val):
+        pass
+
+    def pop(self, val):
+        pass
+
+    def top(self, val):
+        pass
+
+    def minStack(self, val):
+        pass
