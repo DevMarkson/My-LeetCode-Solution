bed412f79be181fb0453651e15031e0cfbb08a73
Markson17 <marksonfrosh@gmail.com>
2026-01-12 20:46:55 +0100

    added a lc question

---
+++ minTimeToVisitAllPoints.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        total = 0
+
+        for i in range(1, len(points)):
+            x1, y1 = points[i - 1]
+            x2, y2 = points[i]
+
+            dx = abs(x2 - x1)
+            dy = abs(y2 - y1)
+
+            total += max(dx, dy)
+        return totala
